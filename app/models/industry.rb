db96e47716f54ad663da9e66769f4a42f31ab2a9
Miguel Tavarez <tavarezster@gmail.com>
2017-05-09 14:21:39 +0000

    validations for account model

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -2,6 +2,6 @@ class Industry < ActiveRecord::Base
   has_many :user_jobs
   has_many :users, :through => :user_jobs
   has_many :jobs, :through => :user_jobs
-
-  # INDUSTRIES = ["Hospitality", "Technology", "Medicine", "Arts", "Government", "Education", "Non-profit", "Finance"]
+  
+  
 end
